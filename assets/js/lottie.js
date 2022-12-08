3c2fcee0f8473c66bc9a6298a216382a0d997944
pramith <malagodap2@gmail.com>
2022-12-08 10:55:55 +0530

    Adding new animation.

--- assets/js/lottie.js
+++ assets/js/lottie.js
@@ -1,7 +1,17 @@
-var animation = bodymovin.loadAnimation({
+var animation1 = bodymovin.loadAnimation({
     // animationData: { /* ... */ },
-    container: document.getElementById('icon-container'), // required
-    path: 'assets/data.json', // required
+    container: document.getElementById('frame1'), // required
+    path: 'assets/data1.json', // required
+    renderer: 'svg', // required
+    loop: true, // optional
+    autoplay: true, // optional
+    name: "Home Animation", // optional
+});
+
+var animation2 = bodymovin.loadAnimation({
+    // animationData: { /* ... */ },
+    container: document.getElementById('frame2'), // required
+    path: 'assets/data2.json', // required
     renderer: 'svg', // required
     loop: true, // optional
     autoplay: true, // optional
